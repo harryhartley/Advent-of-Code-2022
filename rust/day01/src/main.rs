b97db0b5d7cad3e280fcbc442646362db74b36cb
Harry Hartley-Saunders <hhartley-saunders@scottlogic.com>
2022-12-01 10:39:59 +0000

    rust day 1

---
+++ rust/day01/src/main.rs
@@ -0,0 +1,22 @@
+fn main() {
+    let input: &str = include_str!("../input.txt");
+    println!("Silver: {}", silver(input));
+    println!("Gold: {}", gold(input));
+}
+
+fn silver(input: &str) -> i32 {
+    input
+        .split("\n\n")
+        .map(|elf| elf.lines().map(|cals| cals.parse::<i32>().unwrap()).sum())
+        .max()
+        .unwrap_or(0)
+}
+
+fn gold(input: &str) -> i32 {
+    let mut totals: Vec<i32> = input
+        .split("\n\n")
+        .map(|elf| elf.lines().map(|cals| cals.parse::<i32>().unwrap()).sum())
+        .collect();
+    totals.sort();
+    totals.iter().rev().take(3).sum()
+}
