1de234e7aaa84755a9620e7215028112bd6fe40a
Harry Hartley-Saunders <hhartley-saunders@scottlogic.com>
2022-12-04 08:55:41 +0000

    python day 4 bad

---
+++ python/day04/src/main.py
@@ -0,0 +1,28 @@
+def main():
+    input = [l.strip() for l in open("../input.txt").readlines()]
+    print(f"Silver: {silver(input)}")
+    print(f"Gold: {gold(input)}")
+
+def silver(input):
+    total = 0
+    x = [l.split(',') for l in input]
+    for y in x:
+        a = y[0].split('-')
+        b = y[1].split('-')
+        print(a, b)
+        if (int(a[0]) <= int(b[0]) and int(a[1]) >= int(b[1]) or int(a[0]) >= int(b[0]) and int(a[1]) <= int(b[1])):
+            total += 1
+    return total
+
+def gold(input):
+    total = 0
+    x = [l.split(',') for l in input]
+    for y in x:
+        a = y[0].split('-')
+        b = y[1].split('-')
+        print(a, b)
+        if not (int(a[0]) > int(b[1]) or int(a[1]) < int(b[0])):
+            total += 1
+    return total
+
+main()
