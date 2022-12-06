dfc140ed57626d2b95fb515ca93a91aeb62a7ba6
Harry Hartley-Saunders <hhartley-saunders@scottlogic.com>
2022-12-06 09:29:09 +0000

    refactor

--- python/day06/src/main.py
+++ python/day06/src/main.py
@@ -6,21 +6,21 @@ def main():
 def silver(input):
     buffer = []
     for idx, x in enumerate(input):
-        if x not in buffer:
-            buffer.append(x)
-            if len(buffer) == 4:
-                return idx + 1
-        else: buffer = buffer[buffer.index(x)+1:] + [x]
+        if len(buffer) == 4:
+            return idx
+        if x in buffer:
+            buffer = buffer[buffer.index(x)+1:]
+        buffer.append(x)
     return -1
 
 def gold(input):
     buffer = []
     for idx, x in enumerate(input):
-        if x not in buffer:
-            buffer.append(x)
-            if len(buffer) == 14:
-                return idx + 1
-        else: buffer = buffer[buffer.index(x)+1:] + [x]
+        if len(buffer) == 14:
+            return idx
+        if x in buffer:
+            buffer = buffer[buffer.index(x)+1:]
+        buffer.append(x)
     return -1
 
 main()
