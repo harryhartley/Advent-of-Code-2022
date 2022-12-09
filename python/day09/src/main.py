716f7686bc4eb195cb26d4b143105fc9f6b896f9
Harry Hartley-Saunders <hhartley-saunders@scottlogic.com>
2022-12-09 09:36:20 +0000

    pythonic

--- python/day09/src/main.py
+++ python/day09/src/main.py
@@ -1,35 +1,39 @@
-def isTouching(head, tail):
-    return True if abs(head[0] - tail[0]) <= 1 and abs(head[1] - tail[1]) <= 1 else False
+from collections import namedtuple
 
-def getNextTail(head, tail):
-    if isTouching(head, tail): return tail
-    if head[0] == tail[0]: return (head[0], (head[1] + tail[1])//2)
-    if head[1] == tail[1]: return ((head[0] + tail[0])//2, head[1])
-    for x in [(-1, -1), (1, 1), (1, -1), (-1, 1)]:
-        possible = (tail[0] + x[0], tail[1] + x[1])
-        if isTouching(possible, head): return possible
+Coord = namedtuple("Coord", "x y")
 
-def traverse(input):
+def is_touching(head, tail):
+    return True if abs(head.x - tail.x) <= 1 and abs(head.y - tail.y) <= 1 else False
+
+def get_next_tail(head, tail):
+    if is_touching(head, tail): return tail
+    if head.x == tail.x: return Coord(head.x, (head.y + tail.y) // 2)
+    if head.y == tail.y: return Coord((head.x + tail.x) // 2, head.y)
+    for i in [(-1, -1), (1, 1), (1, -1), (-1, 1)]:
+        next_tail = Coord(tail.x + i[0], tail.y + i[1])
+        if is_touching(next_tail, head): return next_tail
+
+def traverse_grid(input):
     visited = [set() for _ in range(9)]
-    head = (0,0)
-    tails = [(0,0) for _ in range(9)]
+    head = Coord(0,0)
+    tails = [Coord(0,0) for _ in range(9)]
     for direction, distance in input:
         for _ in range(int(distance)):
             match direction:
-                case 'U': head = (head[0] + 1, head[1])
-                case 'D': head = (head[0] - 1, head[1])
-                case 'L': head = (head[0], head[1] -1)
-                case 'R': head = (head[0], head[1] + 1)
-            tails[0] = getNextTail(head, tails[0])
+                case 'U': head = Coord(head.x + 1, head.y)
+                case 'D': head = Coord(head.x - 1, head.y)
+                case 'L': head = Coord(head.x, head.y -1)
+                case 'R': head = Coord(head.x, head.y + 1)
+            tails[0] = get_next_tail(head, tails[0])
             for i in range(1, 9):
-                tails[i] = getNextTail(tails[i-1], tails[i])
+                tails[i] = get_next_tail(tails[i-1], tails[i])
             for i in range(9):
                 visited[i].add(tails[i])
     return visited
     
 def main():
     input = [l.split() for l in open('../input.txt').readlines()]
-    visited = traverse(input)
+    visited = traverse_grid(input)
     print(f'Silver: {silver(visited)}')
     print(f'Gold: {gold(visited)}')
 
