2aa38667cce9d632df24fb2974768b4225445315
Harry Hartley-Saunders <hhartley-saunders@scottlogic.com>
2022-12-09 09:41:08 +0000

    coords hard

--- python/day09/src/main.py
+++ python/day09/src/main.py
@@ -1,16 +1,16 @@
 from collections import namedtuple
 
-Coord = namedtuple("Coord", "x y")
+Coord = namedtuple("Coord", "y x")
 
 def is_touching(head, tail):
-    return True if abs(head.x - tail.x) <= 1 and abs(head.y - tail.y) <= 1 else False
+    return True if abs(head.y - tail.y) <= 1 and abs(head.x - tail.x) <= 1 else False
 
 def get_next_tail(head, tail):
     if is_touching(head, tail): return tail
-    if head.x == tail.x: return Coord(head.x, (head.y + tail.y) // 2)
-    if head.y == tail.y: return Coord((head.x + tail.x) // 2, head.y)
+    if head.y == tail.y: return Coord(head.y, (head.x + tail.x) // 2)
+    if head.x == tail.x: return Coord((head.y + tail.y) // 2, head.x)
     for i in [(-1, -1), (1, 1), (1, -1), (-1, 1)]:
-        next_tail = Coord(tail.x + i[0], tail.y + i[1])
+        next_tail = Coord(tail.y + i[0], tail.x + i[1])
         if is_touching(next_tail, head): return next_tail
 
 def traverse_grid(input):
@@ -20,10 +20,10 @@ def traverse_grid(input):
     for direction, distance in input:
         for _ in range(int(distance)):
             match direction:
-                case 'U': head = Coord(head.x + 1, head.y)
-                case 'D': head = Coord(head.x - 1, head.y)
-                case 'L': head = Coord(head.x, head.y -1)
-                case 'R': head = Coord(head.x, head.y + 1)
+                case 'U': head = Coord(head.y + 1, head.x)
+                case 'D': head = Coord(head.y - 1, head.x)
+                case 'L': head = Coord(head.y, head.x -1)
+                case 'R': head = Coord(head.y, head.x + 1)
             tails[0] = get_next_tail(head, tails[0])
             for i in range(1, 9):
                 tails[i] = get_next_tail(tails[i-1], tails[i])
